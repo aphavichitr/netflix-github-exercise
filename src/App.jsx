1ea6b702719677748ab3ae2cfc5508d39f939429
Andrew Phavichitr <andrew@selfycart.com>
2020-04-26 16:34:16 -0700

    Add repo list component and axios

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { TextField } from '@material-ui/core';
+import RepoList from './components/RepoList.jsx';
 
-class App extends React.Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <form>
-            <TextField id="org-text" label="Enter your org" />
-          </form>
-        </header>
-      </div>
-    );
+const App = () => {
+  // state
+  const [org, setOrg] = useState('');
+
+  const enterPressed = (event) => {
+    event.preventDefault()
+
+    if (event.key === 'Enter') {
+      console.log(event)
+    }
   }
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <form>
+          <TextField id="org-text" label="Enter your org" placeholder="netflix" value={org} onChange={setOrg} onKeyDown={enterPressed} />
+        </form>
+
+        <RepoList org={org} />
+      </header>
+    </div>
+  );
 }
 
 export default App;
