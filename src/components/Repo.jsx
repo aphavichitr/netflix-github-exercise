49d56b8c74763643f402f8e8e37bc54949a9b01f
Andrew Phavichitr <andrew.phavichitr@gmail.com>
2020-04-26 18:16:06 -0700

    Add description and icons

--- src/components/Repo.jsx
+++ src/components/Repo.jsx
@@ -1,14 +1,52 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
+import GitHubIcon from '@material-ui/icons/GitHub';
+import StarIcon from '@material-ui/icons/Star';
+
+const useStyles = makeStyles({
+  root: {
+    width: '80%',
+    textAlign: 'center',
+  },
+  title: {
+    fontSize: 14,
+    fontWeight: 'bold',
+  },
+  description: {
+    fontSize: 12,
+  },
+  text: {
+    fontSize: 12,
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  pos: {
+    marginBottom: 12,
+  },
+});
 
 const Repo = (props) => {
+  const classes = useStyles();
   console.log(props.repo)
-
   return (
-    <Card>
+    <Card className={classes.root}>
       <CardContent>
-        {props.repo.name}
+        <GitHubIcon />
+        <Typography className={classes.title} color="textSecondary" gutterBottom>
+          {props.repo.name}
+        </Typography>
+        <Typography className={classes.description} color="textSecondary" gutterBottom>
+          {props.repo.description}
+        </Typography>
+        <Typography className={classes.text} color="textSecondary" gutterBottom>
+          <StarIcon />
+          <span>{props.repo.stargazers_count}</span>
+        </Typography>
       </CardContent>
     </Card>
   );
--- src/components/RepoList.jsx
+++ src/components/RepoList.jsx
@@ -1,14 +1,23 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import Repo from './Repo.jsx';
 
+const useStyles = makeStyles({
+  root: {
+    justifyContent: 'center !important',
+  },
+})
+
 const RepoList = (props) => {
+  const classes = useStyles();
+
   return (
     <div>
       <List>
         { props.repos.map(repo => (
-          <ListItem key={repo.id}>
+          <ListItem className={classes.root} key={repo.id}>
             <Repo repo={repo} />
           </ListItem>
         ))}
