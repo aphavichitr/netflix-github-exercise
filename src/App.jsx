20ae612ce5f1f8268042f4ebd38a52138ce007d3
Andrew Phavichitr <andrew.phavichitr@gmail.com>
2020-04-26 17:54:08 -0700

    Sort repos by starred and add repos to a list and card

--- src/App.jsx
+++ src/App.jsx
@@ -2,28 +2,59 @@ import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { TextField } from '@material-ui/core';
+// import { green } from '@material-ui/core/colors';
 import RepoList from './components/RepoList.jsx';
+import axios from 'axios';
 
 const App = () => {
   // state
   const [org, setOrg] = useState('');
+  const [repos, setRepos] = useState([]);
 
-  const enterPressed = (event) => {
-    event.preventDefault()
+  const handleChange = (event) => {
+    setOrg(event.target.value);
+  }
 
+  const enterPressed = async (event) => {
     if (event.key === 'Enter') {
-      console.log(event)
+      event.preventDefault();
+
+      try {
+        if (org === '') {
+          console.log('error');
+        } else {
+          const result = await axios.get(`https://api.github.com/orgs/${org}/repos`);
+
+          if (result.status === 200) {
+            // sort by starred
+            const sortedRepos = result.data.sort((a, b) => {
+              return b.stargazers_count - a.stargazers_count;
+            })
+
+            setRepos(sortedRepos);
+          }
+        }
+      } catch (err) {
+        console.error(err);
+      }
     }
   }
 
   return (
     <div className="App">
       <header className="App-header">
-        <form>
-          <TextField id="org-text" label="Enter your org" placeholder="netflix" value={org} onChange={setOrg} onKeyDown={enterPressed} />
+        <form className="org-form">
+          <TextField
+            id="org-text"
+            label="Enter your GitHub organization"
+            placeholder="netflix"
+            value={org}
+            fullWidth
+            onChange={handleChange}
+            onKeyDown={enterPressed} />
         </form>
 
-        <RepoList org={org} />
+        <RepoList repos={repos} />
       </header>
     </div>
   );
